3aa2c739efa54f682577b0bb014ce68c3b82d746
nethun5919 <ursula.netherland@bryanuniversity.edu>
2022-09-20 12:36:08 -0700

    changes
    made changes

--- week5/mergesort.py
+++ week5/mergesort.py
@@ -1,14 +1,23 @@
 
-array = [55 ,  31 ,  26 ,  20 ,  63 ,  7 ,  51 ,  74 ,  81 ,  40 ]
+nlist = [55 ,  31 ,  26 ,  20 ,  63 ,  7 ,  51 ,  74 ,  81 ,  40 ]
 
 
 def mergeSort(nlist):
     print("Splitting ",nlist)
     # insert your code to complete the mergeSort function
-    print("Merging ",nlist)
+    if len(nlist) > 1:
+        left_nlist = nlist[:len(nlist)/2]
+        right_nlist = nlist[len(nlist)/2:]
+        
+        mergeSort(left_nlist)
+        mergeSort(right_nlist)
 
+    print("Merging ",nlist)
 def merge(nlist,lefthalf,righthalf):
-    i=j=k=0       
+
+    i= 0
+    j= 0
+    k= 0       
     while i < len(lefthalf) and j < len(righthalf):
         if lefthalf[i] < righthalf[j]:
             nlist[k]=lefthalf[i]
@@ -17,12 +26,10 @@ def merge(nlist,lefthalf,righthalf):
             nlist[k]=righthalf[j]
             j=j+1
         k=k+1
-
     while i < len(lefthalf):
         nlist[k]=lefthalf[i]
         i=i+1
         k=k+1
-
     while j < len(righthalf):
         nlist[k]=righthalf[j]
         j=j+1
