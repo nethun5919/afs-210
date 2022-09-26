05f774b779626d313f92d6b8a21b7971c82e1dad
nethun5919 <ursula.netherland@bryanuniversity.edu>
2022-09-26 16:03:22 -0700

    week6

--- week4/hashing.py
+++ week4/hashing.py
@@ -6,22 +6,18 @@ class HashTable:
    
     def hashFunction(self,key):
       #Insert your hashing function code
+
        #key modulus table_size
 
       key % self.size
-         # multi = 1
-         # hv = 0
-         # for ch in str(key):
-         #    hv += multi * ord(ch)
-         #    multi += 1
-         #    return key % self.size
-
+      
     def rehash(self,key):
         # Insert your secondary hashing function codye
      # key div table_size
      pass
      def put(self,key,data):
         # Insert your code here to store key and data 
+
          hv = self.hashFunction(key) 
          if self.slots [hv]== None:
             self.slots[hv] = key
@@ -35,17 +31,19 @@ class HashTable:
                   self.slots[hv] = key
                   self.data[hv] = data
                else:
-                  print("unresolved collision")
+                  hv = self.rehash(key)
+                  if self.slots [hv]== None:
+                     self.slots[hv] = key
+                     self.data [hv] = data
+                  else:
+                   print("unresolved collision")
 
 
-        
-        
-
-    def get(self,key):
+         def get (self,key):
         # Insert your code here to get data by key
     
-     def __getitem__ (self,key):
-        return self.get(key)
+            def __getitem__ (self,key):
+               return self.get(key)
     
      def __setitem__ (self,key,data):
         self.put(key,data)
--- week5/mergesort.py
+++ week5/mergesort.py
@@ -1,14 +1,24 @@
 
-array = [55 ,  31 ,  26 ,  20 ,  63 ,  7 ,  51 ,  74 ,  81 ,  40 ]
-
+nlist = [55 ,  31 ,  26 ,  20 ,  63 ,  7 ,  51 ,  74 ,  81 ,  40 ]
 
 def mergeSort(nlist):
     print("Splitting ",nlist)
     # insert your code to complete the mergeSort function
-    print("Merging ",nlist)
+    if len(nlist) > 1:
+        left_nlist = nlist[:len(nlist)//2]
+        right_nlist = nlist[len(nlist)//2:]
+        
+        mergeSort(left_nlist)
+        mergeSort(right_nlist)
 
+        nlist = merge(nlist, left_nlist, right_nlist)
+
+    print("Merging ",nlist)
 def merge(nlist,lefthalf,righthalf):
-    i=j=k=0       
+
+    i= 0
+    j= 0
+    k= 0       
     while i < len(lefthalf) and j < len(righthalf):
         if lefthalf[i] < righthalf[j]:
             nlist[k]=lefthalf[i]
@@ -17,14 +27,16 @@ def merge(nlist,lefthalf,righthalf):
             nlist[k]=righthalf[j]
             j=j+1
         k=k+1
-
     while i < len(lefthalf):
         nlist[k]=lefthalf[i]
         i=i+1
         k=k+1
-
     while j < len(righthalf):
         nlist[k]=righthalf[j]
         j=j+1
         k=k+1
     return nlist
+
+print(nlist)
+mergeSort(nlist)
+print(nlist)
